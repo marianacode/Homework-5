df2d73ad85a400f59782a27c746169e99b403390
Mariana Robertson <marygmaciel@gmail.com>
2020-07-24 17:11:59 -0700

    Homework-5

--- Script.js
+++ Script.js
@@ -17,4 +17,36 @@ $("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
 
 $(document).ready(function () {
 
+    renderPlans();
+
+    $("#currentDay").append();
+
+    function addDate() {
+        $("#currentDay").html(moment().format("MMMM Do YYYY, h:mm a"));
+
+    } setInterval(addDate, 1000);
+
+
+    // Change color 
+
+    for (var i = 0; i <= 12; i++) { 
+
+        var inputHour = $("#" + "Row").attr("data-time");
+        var inputHouInt = parseInt(inputHour);
+
+        if (currentHourInt === inputHouInt) { 
+            $("#" + i + "Row").addClass("present");   
+        }
+
+        if (currentHourInt > inputHouInt) { 
+            $("#" + i + "Row").addClass("past");   
+        }
+
+        if (currentHourInt < inputHouInt) { 
+            $("#" + i + "Row").addClass("future");   
+        }
+
+
+    }
+
 }
