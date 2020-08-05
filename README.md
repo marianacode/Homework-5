c012203b551ff955bfb9bf186b56c778de64f501
Mariana Robertson <marygmaciel@gmail.com>
2020-08-05 15:51:17 -0700

    Homework-5 updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Homework-5
+
+Calendar application 
+
+User can add and save tasks for each work hour of the day.
+
+Type the task and click save.
+
+Red is the current hour.
+Grey is past hour(s).
+Green is the future hour(s).
+
+
+
--- Script.js
+++ Script.js
@@ -1,13 +1,15 @@
-// variables
-var saveBtn = $(".saveBtn");
-var currentHour = moment().format("HH");
-var currentHourInt = parseInt(currentHour);
+// // variables
 
-// Set attributes
+var saveBtn = $(".saveBtn");
+var currentHour = moment().format("HH"); 
+var currentHourInt = parseInt(currentHour); 
+
+// Attributes 
+
 $("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
-$("#10Row").attr("data-time", moment("10:00 am", "h:mm a").format("HH"));
-$("#11Row").attr("data-time", moment("11:00 am", "h:mm a").format("HH"));
-$("#12Row").attr("data-time", moment("12:00 pm", "h:mm a").format("HH"));
+$("#10Row").attr("data-time", moment("10:00 am", "hh:mm a").format("HH"));
+$("#11Row").attr("data-time", moment("11:00 am", "hh:mm a").format("HH"));
+$("#12Row").attr("data-time", moment("12:00 pm", "hh:mm a").format("HH"));
 $("#1Row").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
 $("#2Row").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
 $("#3Row").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
@@ -17,36 +19,48 @@ $("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
 
 $(document).ready(function () {
 
+  
     renderPlans();
 
-    $("#currentDay").append();
 
-    function addDate() {
-        $("#currentDay").html(moment().format("MMMM Do YYYY, h:mm a"));
+  $("#currentDay").append();
 
-    } setInterval(addDate, 1000);
+  function addDate() { 
+  $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm a'));
+  
+  } setInterval(addDate, 1000);
 
+//  Colors 
 
-    // Change color 
+  for (var i = 0; i <= 12; i++) {  
 
-    for (var i = 0; i <= 12; i++) { 
+      var inputHour = $("#" + i + "Row").attr("data-time"); 
+      var inputHourInt = parseInt(inputHour); 
 
-        var inputHour = $("#" + "Row").attr("data-time");
-        var inputHouInt = parseInt(inputHour);
-
-        if (currentHourInt === inputHouInt) { 
-            $("#" + i + "Row").addClass("present");   
-        }
-
-        if (currentHourInt > inputHouInt) { 
-            $("#" + i + "Row").addClass("past");   
-        }
-
-        if (currentHourInt < inputHouInt) { 
-            $("#" + i + "Row").addClass("future");   
-        }
-
-
+      if (currentHourInt === inputHourInt) {
+          $("#" + i + "Row").addClass("present"); 
+      }
+      if (currentHourInt > inputHourInt) { 
+          $("#" + i + "Row").addClass("past");
+      }
+      if (currentHourInt < inputHourInt) { 
+          $("#" + i + "Row").addClass("future");
+      }
     }
 
-}
+
+//   Local Storage 
+
+    saveBtn.on("click", function () { 
+
+      var rowHour = $(this).attr("data-hour"); 
+      var input = $("#" + rowHour + "Row").val(); 
+    });
+  
+    //  
+    function renderPlans() {
+      for (var i = 0; i <= 12; i++) {
+      $("#" + i + "Row").val(localStorage.getItem(i));
+      }
+    }
+});
